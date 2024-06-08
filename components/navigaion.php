8b7bfa9f67e3fb14e2fb644f120f4dc497a6070b
vincent9579 <vincent9579@gmail.com>
2024-06-08 17:17:54 +0800

    fix(navigation): 錯誤的名稱

--- components/navigaion.php
+++ components/navigaion.php
@@ -66,7 +66,7 @@
                 <?php
                 if ($status == "valid" && $is_admin == 'Y') {
                     echo '<li>
-                        <a href="admin.php"
+                        <a href="admin_center.php"
                             class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">管理介面</a>
                     </li>';
                 }
