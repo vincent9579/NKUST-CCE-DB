c9731e4bd4970871806e1999ffb468738b38203b
Naivezx <145947783+Naivezx@users.noreply.github.com>
2024-06-16 21:11:33 +0800

    Update register.php

--- register.php
+++ register.php
@@ -40,43 +40,43 @@ if (!isset($_SESSION['username'])) {
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1
                             class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Create an account
+                            建立帳戶
                         </h1>
                         <form class="space-y-4 md:space-y-6" action="reg.php" method="POST">
                             <div class="grid grid-cols-2 gap-4">
                                 <div>
                                     <label for="user_name"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">使用者名稱</label>
                                     <input type="text" name="user_name" id="user_name"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="johndoe" required>
                                 </div>
                                 <div>
                                     <label for="std_name"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full
-                                        Name</label>
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">真實名稱
+                                    </label>
                                     <input type="text" name="std_name" id="std_name"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="John Doe" required>
                                 </div>
                                 <div>
                                     <label for="std_id"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Student
-                                        ID</label>
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">學號
+                                        </label>
                                     <input type="text" name="std_id" id="std_id"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="201900000" required>
                                 </div>
                                 <div>
                                     <label for="std_departments"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Department</label>
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">系所</label>
                                     <input type="text" name="std_departments" id="std_departments"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="Computer Science" required>
                                 </div>
                                 <div>
                                     <label for="password"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">密碼</label>
                                     <input type="password" name="user_password" id="user_password"
                                         placeholder="••••••••"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -84,8 +84,7 @@ if (!isset($_SESSION['username'])) {
                                 </div>
                                 <div>
                                     <label for="confirm-password"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
-                                        password</label>
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">確認密碼</label>
                                     <input type="password" name="confirm_password" id="confirm-password"
                                         placeholder="••••••••"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -99,18 +98,17 @@ if (!isset($_SESSION['username'])) {
                                         required>
                                 </div>
                                 <div class="ml-3 text-sm">
-                                    <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the
-                                        <a class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                                            href="#">Terms and Conditions</a></label>
+                                    <label for="terms" class="font-light text-gray-500 dark:text-gray-300">我不是機器人
+                                    </label>
                                 </div>
                             </div>
                             <button type="submit"
-                                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create
-                                an account</button>
+                                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">建立
+                                帳戶</button>
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Already have an account? <a href="login.html"
-                                    class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login
-                                    here</a>
+                                已經註冊? <a href="login.html"
+                                    class="font-medium text-primary-600 hover:underline dark:text-primary-500">登入
+                                    </a>
                             </p>
                         </form>
                     </div>
